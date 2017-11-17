fb3b41aa3f69ec39f7d0684255212028b6662561
Nick Morrow <nmorrow11@gmail.com>
2017-11-16 19:09:40 -0800

    setting up heroku, it runs through heroku locally

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fetch = require('./fetchYouTubeVideo');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = require('socket.io').listen(server);
 
